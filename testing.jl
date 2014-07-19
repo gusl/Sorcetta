8057ab01e95eeb90767a261d3cee84b509b3abff
Gustavo Lacerda <gusg@optimizelife.com>
2014-07-19 01:33:22 -0400

    handling python errors;  translating literals

--- testing.jl
+++ testing.jl
@@ -9,8 +9,8 @@
 
 
 include("types.jl")
-
 require("Iterators")
+using PyCall
 ##using Rif
 ##initr()
 
@@ -30,11 +30,10 @@ end
 
 ## R_out = eval_R_code("a<-1; a+1", int)
 
-
-function bind_vars(dict, expr::String)
+function bind_vars(dict, expr::String, translate_fn=identity)
     for key = collect(keys(dict))
-        val = dict[key]
-        exp = parse(expr)
+        val = translate_fn(dict[key])
+        ##exp = parse(expr)
         expr = replace(expr, key, "($val)")
     end
     expr
@@ -43,117 +42,16 @@ end
 dict = ["a" => 1]
 bind_vars(dict, "a+1")
 
-
-## compare R computation with Julia computation on every input
-function test(J::String, M::String, P::String, R::String, input::Dict)
-    num_pass = 0;
-    dict = input
-    if J!=""
-        Julia_code_bound = bind_vars(dict, J)
-        julia_out = eval(parse(Julia_code_bound))
-        println("julia_out = $julia_out")
-    end
-    if P!=""
-        python_code_bound = bind_vars(dict, P)
-        python_out = pyeval(python_code_bound)
-        println("python_out = $python_out")
-    end
-    if R!=""
-        R_code_bound = bind_vars(dict, R)
-        R_out = eval_R_code(R_code_bound)
-        R_out = eval(parse(R_out))
-        println("R_out = $R_out")
-    end
-    pass = isequal(R_out, julia_out) || R_out==julia_out
-    if pass
-    ##if ("$R_out"=="$julia_out") ## string equality, viz NaN
-        println("Passed")
-    else
-        println("Failed")
-    end
-    pass
+python_literals = {"Inf" => "float('inf')"}
+python_literals["Inf"]
+function translate_literal_to_python(literal)
+    (literal in keys(python_literals)) ? python_literals[literal] : literal
 end
 
-
-## function test(R_code::String, Julia_code::String, inputs::Array{Dict})
-##     num_pass = 0;
-##     R_outs = Array[];
-##     julia_outs = Array[];
-##     for (i,dict) = enumerate(inputs)
-##         println("i = $i")
-##         print(dict)
-##         Julia_code_bound = bind_vars(dict, Julia_code)
-##         R_code_bound = bind_vars(dict, R_code)
-##         julia_out = eval(parse(Julia_code_bound))
-##         ##push!(julia_outs, julia_out)
-##         println("julia_out = $julia_out")
-##         R_out = eval_R_code(R_code_bound)
-##         R_out = eval(parse(R_out))
-##         ##push!(R_outs, R_out)
-##         println("R_out = $R_out")
-##         if (R_out==julia_out)
-##             num_pass = num_pass + 1
-##         end
-##     end
-##     println("Passed: $num_pass;  Failed: ")
-##     (julia_outs, R_outs)
-## end
 
 remove_spaces(s) = replace(replace(s, " ",""), "\n", "")
 is_code(s) = length(s)>2 && s[2:3]==": " && s[1]!="#"
 is_precondition(s) = length(s)>4 && s[1:4]=="PRE:"
-
-## ## load
-## function read_Rjl(filename::String)
-##     f=open("$filename.Rjl")
-##     entries=T.Entry[]
-##     inputs=T.Input[]
-##     line = nothing; section=nothing;
-##     R=""; J=""; precond="";
-##     ##Entries
-##     while true
-##         line=readline(f)
-##         if section=="end"
-##             break
-##         end
-##         println(line)
-##         nline=remove_spaces(line)
-##         if(length(nline)>2 && nline[1:2]=="==")
-##             section=lowercase(replace(nline,"==",""))
-##             continue
-##         end
-##         if(section=="code")
-##             if (nline=="")
-##                 push!(entries, T.Entry(R,J,"","",precond))
-##                 R = ""; J = "";
-##             elseif (is_code(line))
-##                 lang = line[1:1]
-##                 code = nline[3:end]
-##                 if (lang=="R")
-##                     R = code
-##                 end
-##                 if (lang=="J")
-##                     J = code
-##                 end
-##             elseif (is_precondition(line))
-##                 precond = replace(nline, "PRE:", "")
-##             end
-##         end
-##         if(section=="inputs" && nline!="")
-##             ss = split(line, " ")
-##             println("ss = $ss")
-##             var = ss[1]
-##             valString = ss[2]
-##             println("valString = $valString")
-##             vals = split(valString, ",")
-##             push!(inputs, T.Input(var,vals))
-##         end
-##         println("entries = $entries")
-##         println("inputs = $inputs")
-##     end
-##     ## Inputs
-##     (entries, inputs)
-## end
 
 
 function read_JMPR(filename::String)
@@ -163,8 +61,7 @@ function read_JMPR(filename::String)
     line = nothing; section=nothing;
     J=""; M=""; P=""; R=""; precond="";
     ##Entries
-    while true
-        line=readline(f)
+    for line in map(cleanup, readlines(f))
         if section=="end"
             break
         end
@@ -229,8 +126,49 @@ end
 ## bind_and_eval("a+b", dict)
 
 
+input = {:a=>1, :b=>1}
+J="a+b"
 
-function run_test(entry::T.Entry, vars, vals, langs)
+## compare R computation with Julia computation on every input
+function test(J::String, M::String, P::String, R::String, input::Dict)
+    num_pass = 0;
+    outputs = String[]
+    if J!=""
+        Julia_code_bound = bind_vars(input, J)
+        julia_out = eval(parse(Julia_code_bound))
+        println("julia_out = $julia_out")
+        push!(outputs, "$julia_out")
+    end
+    if P!=""
+        python_code_bound = bind_vars(input, P, translate_literal_to_python)
+        println("python_code_bound = $python_code_bound")
+        python_out = try
+            pyeval(python_code_bound)
+        catch
+            "PythonError"
+        end
+        println("python_out = $python_out")
+        push!(outputs, "$python_out")
+    end
+    if R!=""
+        R_code_bound = bind_vars(input, R)
+        R_out = eval_R_code(R_code_bound)
+        R_out = eval(parse(R_out))
+        println("R_out = $R_out")
+        push!(outputs, "$R_out")
+    end
+    pass = isequal(R_out, julia_out) || R_out==julia_out
+    if pass
+    ##if ("$R_out"=="$julia_out") ## string equality, viz NaN
+        println("Passed")
+    else
+        println("Failed")
+    end
+    (outputs, pass)
+end
+
+
+function run_test(entry::T.Entry, vars, vals, langs::Set)
     dict = Dict() ## variable bindings into Dict format
     for (i, var) = enumerate(vars)
         dict[var] = vals[i]
@@ -243,6 +181,10 @@ function run_test(entry::T.Entry, vars, vals, langs)
         println("pre-condition not met.")
         return(nothing)
     else
+        entry.J = ("J" in langs) ? entry.J : ""
+        entry.M = ("M" in langs) ? entry.M : ""
+        entry.P = ("P" in langs) ? entry.P : ""
+        entry.R = ("R" in langs) ? entry.R : ""
         test(entry.J, entry.M, entry.P, entry.R, dict)
     end
 end
@@ -253,13 +195,25 @@ function run_tests(entries, inputs, outfile, langs::Set)
     vars = map(input->input.var, inputs)
     valss = map(input->input.vals, inputs)
     f = open("$outfile.out", "w")
+    langs_str = reduce((s1,s2)->s1*" ;; "*s2,  langs)
+    lang_outputs = reduce((s1,s2)->s1*" ;; "*s2, map(s->s*"_output", langs))
+    header = langs_str*" ;; input ;; "*lang_outputs*" ;; passed?"
+    println(f, header)
     for entry in entries
         for vals in apply(Iterators.product, valss)
             println(entry)
             println(vals) ##bind_vars()
-            outcome = run_test(entry, vars, vals, langs)
+            (outputs,outcome) = run_test(entry, vars, vals, langs)
             ## write output file
-            out_line = "$(entry.R) ;; $(entry.J) ;; $vals ;; $outcome"
+            out_line = ""
+            for lang in langs
+                out_line *= getfield(entry, symbol(lang))*" ;; "
+            end
+            out_line *= " $vals ;;"
+            for output in outputs
+                out_line *= " $output ;; "
+            end
+            out_line *= "$outcome"
             println(f, out_line)
         end
     end
@@ -268,21 +222,11 @@ end
 
 
 (entries,inputs) = read_JMPR("arith")
-langs = Set({"R","J"})
+langs = Set({"R", "J", "P"})
 run_tests(entries, inputs, "arith", langs)
 
 
 
-(entries,inputs) = read_Rjl("vectors")
-run_tests(entries, inputs, "vectors")
-
-
-
-
-
-df = DataFrame()
-    df[:Julia] = julia_outs
-    df[:R] = R_outs
 
 
 code0_R = "a<-1; a+1";
@@ -290,20 +234,4 @@ code0_Julia = "a=1; a+1"
 
 code1_R = "a+1";
 code1_Julia = "a+1"
-
-
-test(code1_R, code1_Julia, ["a" => 2])
-test(code1_R, code1_Julia, ["a" => -5])
-
-
-inputs = Dict[]
-push!(inputs, ["a" => -5])
-push!(inputs, ["a" => -1])
-push!(inputs, ["a" => 0])
-push!(inputs, ["a" => 1])
-push!(inputs, ["a" => 1e10])
-
-test(code1_R, code1_Julia, inputs)
-
-
 
